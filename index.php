7713bc53dbef3f7ec656669b396818a28be10b20
Quant & Fintech Opensource Projects <reunisoft@gmail.com>
2019-12-18 10:53:47 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+$filename = "rssfeeds.txt";
+$arr = file("rsslinks.txt", FILE_IGNORE_NEW_LINES);
+
+if (isset($_GET['id_feed'])) {
+    echo "<a href='/'>Back to main</a><br/><br/>";
+    $id_feed = $_GET['id_feed'];
+    getFeed($arr[$id_feed]);
+    exit;
+}
+
+$id_feed = 0;
+foreach ($arr as &$value) {
+try {
+    getFeedTitle($value, $id_feed);
+    $id_feed = $id_feed+1;
+}catch(exception $e){
+    echo "$e";
+}
+}
+
+exit;
+
+function getFeedTitle($feed_url, $id_feed) {
+
+    $content = file_get_contents($feed_url);
+    $x = new SimpleXmlElement($content);
+    
+    $id = 0;
+    foreach($x->channel as $entry) {
+        $fulltitle = "$entry->title ";
+        if ($entry->lastBuildDate<>"") $fulltitle = $fulltitle . "($entry->lastBuildDate)";
+        echo "<a href='?id_feed=$id_feed'>$fulltitle</a><br/>";
+        $id = $id+1;
+    }
+    
+}
+
+function getFeed($feed_url) {
+     
+    $content = file_get_contents($feed_url);
+    $x = new SimpleXmlElement($content);
+    
+    foreach($x->channel as $entry) {
+        echo "<h3>$entry->title ";
+        if ($entry->lastBuildDate<>"") echo "($entry->lastBuildDate)";
+        echo "</h3>";
+    }
+    
+    echo "<ul>";
+     
+    foreach($x->channel->item as $entry) {
+        echo "<li>$entry->pubDate : <a href='$entry->link' title='$entry->title'>" . $entry->title . "</a></li>";
+        if ($entry->description <> "") {
+            echo "$entry->description";
+            echo "<br/><br/>";
+        } else {
+        }
+    }
+    
+    echo "</ul>";
+    
+}
+
+?>
