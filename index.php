8536521f9f498e9ddd1e0e8a46bb0431e9bca1eb
Quant & Fintech Opensource Projects <reunisoft@gmail.com>
2019-12-19 15:58:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
+// echo "<img src='https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-images/2019-11/f3fe64e0-06ee-11ea-a7b7-c13b44577ec9' width='125px' height='56px'/>";
+
+echo "<html><head><title>Autoblog</title></head><body>";
+
 $arr = file("rsslinks.txt", FILE_IGNORE_NEW_LINES);
 
 if (isset($_GET['id_feed'])) {
     echo "<a href='/'>Back to main</a><br/><br/>";
     $id_feed = $_GET['id_feed'];
     getFeed($arr[$id_feed]);
+    
+    echo "</body></html>";
     exit;
 }
 
@@ -21,10 +27,12 @@ try {
 }
 }
 
+echo "</body></html>";
 exit;
 
 function getFeedTitle($feed_url, $id_feed) {
 
+    try{
     $content = file_get_contents($feed_url);
     $x = new SimpleXmlElement($content);
     
@@ -32,6 +40,9 @@ function getFeedTitle($feed_url, $id_feed) {
         $fulltitle = "$entry->title ";
         if ($entry->lastBuildDate<>"") $fulltitle = $fulltitle . "($entry->lastBuildDate)";
         echo "<a href='?id_feed=$id_feed'>$fulltitle</a><br/>";
+    }
+    }catch(exception $e){
+        
     }
     
 }
